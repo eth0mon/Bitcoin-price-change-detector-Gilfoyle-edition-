2c43fbe1d859969dc5d9d4c14a6f7a1968b9dd52
eth0 <36668598+eth0mon@users.noreply.github.com>
2020-05-31 15:52:03 +0400

    Add files via upload

---
+++ bitgilf.py
@@ -0,0 +1,29 @@
+import urllib.request, json
+from playsound import playsound
+ 
+target = 9440
+change = True
+def value():
+    with urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json") as url:
+        data = json.loads(url.read().decode())
+    return float(data.get('bpi').get('USD').get('rate').replace(",", ""))
+price_old = value()
+ 
+while True:
+    price = value()
+    if price <=target and price_old>=target:
+        change = True
+        price_old = price
+    elif price >= target and price_old<=target:
+        change = True
+        price_old = price
+       
+    while change:
+        price = value()
+        if price <= target:
+            print("FUCK YOU DINESH\n")
+            change = True
+        else:
+            print("YOU SUFFER BUT WHY\n")
+            playsound('u.mp3')
+            change = False
